c2dc9996cfbe004473a920802141910c0769a229
Raphael Martins <raphiusk.rio@gmail.com>
2022-05-16 19:35:56 -0300

    Query Params

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
   #Caso faça o front no rails, existe um arquivo em 'views' com nome da 'action', neste caso 'index.html.erb'
   def index
-    @nome = "Raphael" #variável de sessão/instância
+    @meu_nome = "Raphael" #variável de sessão/instância
+    #variável recebendo parâmetro pela URL. É importante ressaltar que o symbol dentro do params não precisa ser necessariamente igual a variável
+    @meu_curso = params[:course]
+    #utilizar & para enviar mais de um parâmetro pela URL
+    @meu_nivel = params[:level]
   end
 end
