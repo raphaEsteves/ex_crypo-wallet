57f9e711b6ad0ab79d209b9210139236619eebb7
Raphael Martins <raphiusk.rio@gmail.com>
2022-05-16 19:08:02 -0300

    Instance variable

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
+  #Caso faça o front no rails, existe um arquivo em 'views' com nome da 'action', neste caso 'index.html.erb'
   def index
+    @nome = "Raphael" #variável de sessão/instância
   end
 end
